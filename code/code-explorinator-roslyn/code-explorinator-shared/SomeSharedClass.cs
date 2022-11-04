b4597218616f93c6dce9c44cbbde87412164e8d2
vero <v.struemper@gmail.com>
2022-11-04 16:14:15 +0100

    setup files von cameron

---
+++ code/code-explorinator-roslyn/code-explorinator-shared/SomeSharedClass.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeExplorinator; 
+
+// Classes in the shared project can be used by both the generator project and the unity project.
+// Classes defined in the generator project can *NOT* be used in the unity project; only the generated sources can.
+
+public class SomeSharedClass {
+    public const string HelloWorld = "Hello World!";
+}
