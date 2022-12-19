5f8ca7147ef90a9a299788dfd10ff647b7eba66d
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2022-12-19 19:37:30 +0100

    .gitignore korrigiert

--- code/code-explorinator-roslyn/code-explorinator-shared/SomeSharedClass.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodeExplorinator; 
-
-// Classes in the shared project can be used by both the generator project and the unity project.
-// Classes defined in the generator project can *NOT* be used in the unity project; only the generated sources can.
-
-public class SomeSharedClass {
-    public const string HelloWorld = "Hello World!";
-}
