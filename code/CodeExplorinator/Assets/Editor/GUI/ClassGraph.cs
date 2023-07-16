046deb9bfd938bed2e3def81e333eadf9e726586
Yannik <yannik.stamm@stud-mail.uni-wuerzburg.de>
2023-07-16 15:16:22 +0200

    Added: Arrows that indicate further dependencies of classes Removed: Old Arrows that point toward random position Refactored: parts of ConnectionGUI and parts of ClassGraph

--- code/CodeExplorinator/Assets/Editor/GUI/ClassGraph.cs
+++ code/CodeExplorinator/Assets/Editor/GUI/ClassGraph.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -61,137 +62,82 @@ namespace CodeExplorinator
 
         public void GenerateConnectionsBetweenClasses()
         {
-            foreach (ClassNode foot in classNodes)
+            foreach(ClassNode node in classNodes.Where(x => x.IsLeaf))
             {
-                if (foot.IsLeaf)
+                int connectionsThatLeadOutsideTheGraph = 0;
+                int connectionsThatOriginateOutsideTheGraph = 0;
+                foreach(ClassNode otherNode in node.outgoingConnections)
                 {
-                    foreach (ClassNode tip in foot.outgoingConnections)
+                    if(classNodes.Contains(otherNode) == false)
                     {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
-
-                        VisualElement shownTip = classNodes.Contains(tip) ? tip.classGUI.VisualElement : null;
-                        ConnectionGUI connection = new ConnectionGUI(graphManager, foot.classGUI.VisualElement, shownTip);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
+                        connectionsThatLeadOutsideTheGraph++;
+                        continue;
                     }
 
-                    foreach (ClassNode tip in foot.ingoingConnections) //tip and foot here have opposite meanings
-                    {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
+                    ConnectionGUI connection = new ConnectionGUI(graphManager, node.classGUI.VisualElement, otherNode.classGUI.VisualElement);
+                    connections.Add(connection);
+                }
 
-                        VisualElement shownTip = classNodes.Contains(tip) ? tip.classGUI.VisualElement : null;
-                        ConnectionGUI connection = new ConnectionGUI(graphManager, shownTip,
-                            foot.classGUI.VisualElement);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
-                    }
-
-                    foreach (var parent in foot.ClassData.ExtendingOrImplementingClasses)
+                foreach (ClassNode otherNode in node.ingoingConnections)
+                {
+                    if (classNodes.Contains(otherNode) == false)
                     {
-                        ConnectionGUI connection = new ConnectionGUI(graphManager, foot.classGUI.VisualElement,
-                            classNodes.Contains(parent.ClassNode)
-                                ? parent.ClassNode.classGUI.VisualElement
-                                : null, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
-                    }
-
-                    foreach (var child in
-                             foot.ClassData.ChildClasses) //tip(aka child) and foot here have opposite meanings
-                    {
-                        ConnectionGUI connection = new ConnectionGUI(graphManager,
-                            classNodes.Contains(child.ClassNode) ? child.ClassNode.classGUI.VisualElement : null,
-                            foot.classGUI.VisualElement, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
+                        connectionsThatOriginateOutsideTheGraph++;
+                        continue;
                     }
                 }
-                else
+
+                foreach (ClassData otherNode in node.ClassData.ExtendingOrImplementingClasses)
                 {
-                    foreach (ClassNode tip in foot.outgoingConnections)
+                    if(classNodes.Contains(otherNode.ClassNode) == false)
                     {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
-
-                        ConnectionGUI connection =
-                            new ConnectionGUI(graphManager, foot.classGUI.VisualElement, tip.classGUI.VisualElement);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
+                        connectionsThatLeadOutsideTheGraph++;
+                        continue;
                     }
 
-                    foreach (var parent in foot.ClassData.ExtendingOrImplementingClasses)
+                    ConnectionGUI connection = new ConnectionGUI(graphManager, node.classGUI.VisualElement, 
+                                                                otherNode.ClassNode.classGUI.VisualElement, true);
+                    connections.Add(connection);
+                }
+
+                foreach (ClassData otherNode in node.ClassData.ChildClasses)
+                {
+                    if (classNodes.Contains(otherNode.ClassNode) == false)
                     {
-                        ConnectionGUI connection = new ConnectionGUI(graphManager, foot.classGUI.VisualElement,
-                            parent.ClassNode.classGUI.VisualElement, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
+                        connectionsThatOriginateOutsideTheGraph++;
+                        continue;
                     }
+                }
+
+                if(connectionsThatLeadOutsideTheGraph != 0)
+                {
+                    ConnectionGUI implicatingArrowAway = new ConnectionGUI(graphManager, node.classGUI.VisualElement, false);
+                    connections.Add(implicatingArrowAway);
+                }
+
+                if(connectionsThatOriginateOutsideTheGraph != 0)
+                {
+                    ConnectionGUI implicatingArrowToward = new ConnectionGUI(graphManager, node.classGUI.VisualElement, true);
+                    connections.Add(implicatingArrowToward);
+                }
+            }
+
+            foreach (ClassNode node in classNodes.Where(x => !x.IsLeaf))
+            {
+                foreach (ClassNode otherNode in node.outgoingConnections)
+                {
+                    ConnectionGUI connection = new ConnectionGUI(graphManager, node.classGUI.VisualElement, otherNode.classGUI.VisualElement);
+                    connections.Add(connection);
+                }
+
+                foreach (ClassData otherNode in node.ClassData.ExtendingOrImplementingClasses)
+                {
+                    ConnectionGUI connection = new ConnectionGUI(graphManager, node.classGUI.VisualElement,
+                                                                 otherNode.ClassNode.classGUI.VisualElement, true);
+                    connections.Add(connection);
                 }
             }
         }
-
-        /*
-        //this code could be obsolete
-        public void GenerateConnectionsBetweenMethods(HashSet<MethodNode> shownMethodNodes)
-        {
-            foreach (MethodNode foot in shownMethodNodes)
-            {
-                if (foot.IsLeaf)
-                {
-                    foreach (MethodNode tip in foot.outgoingConnections)
-                    {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
-
-                        VisualElement shownTip = shownMethodNodes.Contains(tip) ? tip.MethodGUI.VisualElement : null;
-                        ConnectionGUI connection = new ConnectionGUI(graphManager, foot.MethodGUI.VisualElement,
-                            shownTip, false, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
-                    }
-
-                    foreach (MethodNode tip in foot.ingoingConnections) //tip and foot here have opposite meanings
-                    {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
-
-                        VisualElement shownTip = shownMethodNodes.Contains(tip) ? tip.MethodGUI.VisualElement : null;
-                        ConnectionGUI connection = new ConnectionGUI(graphManager,shownTip,
-                            foot.MethodGUI.VisualElement, false, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
-                    }
-                }
-                else
-                {
-                    foreach (MethodNode tip in foot.outgoingConnections)
-                    {
-                        if (foot == tip)
-                        {
-                            continue;
-                        }
-
-                        ConnectionGUI connection =
-                            new ConnectionGUI(graphManager, foot.MethodGUI.VisualElement, tip.MethodGUI.VisualElement, false, true);
-                        connection.GenerateVisualElement();
-                        connections.Add(connection);
-                    }
-                }
-            }
-        }
-        */
 
         public void GenerateMethodConnectionsBetweenClasses()
         {
@@ -205,7 +151,6 @@ namespace CodeExplorinator
                      }
                      
                      ConnectionGUI connection = new ConnectionGUI(graphManager, foot.classGUI.VisualElement, tip.classGUI.VisualElement, false, false, false);
-                     connection.GenerateVisualElement();
                      connections.Add(connection);
                  }
              }
@@ -213,7 +158,6 @@ namespace CodeExplorinator
 
         public bool Contains(ClassGUI classGUI)
         {
-            bool isPartOfGraph = false;
             foreach(ClassNode node in classNodes)
             {
                 if(node.classGUI == classGUI)
--- code/CodeExplorinator/Assets/Editor/GUI/ConnectionGUI.cs
+++ code/CodeExplorinator/Assets/Editor/GUI/ConnectionGUI.cs
@@ -11,18 +11,55 @@ namespace CodeExplorinator
 {
     public class ConnectionGUI : BaseGUI
     {
+        protected static Texture2D LineTexture
+        {
+            get
+            {
+                if(lineTexture == null)
+                {
+                    lineTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/Linetexture.png");
+                }
+                return lineTexture;
+            }
+        }
+        protected static Texture2D ArrowTexture
+        {
+            get
+            {
+                if (arrowTexture == null)
+                {
+                    arrowTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/pfeil_centered_new.png");
+                }
+                return arrowTexture;
+            }
+        }
+        protected static Texture2D InheritanceArrowTexture
+        {
+            get
+            {
+                if (inheritanceArrowTexture == null)
+                {
+                    inheritanceArrowTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/Arrow_inheritance.png");
+                }
+                return inheritanceArrowTexture;
+            }
+        }
+
         public VisualElement FootNode { get; private set; }
         public VisualElement TipNode { get; private set; }
+
+        private const float indicatingArrowLength = 100;
+        private const float indicatingArrowsOffset = 50;
+        private const float indicatingNumberOffset = 20;
+        private static Texture2D lineTexture;
+        private static Texture2D arrowTexture;
+        private static Texture2D inheritanceArrowTexture;
 
         private bool isInheritanceConnection;
         private bool setCenterLeft;
         private bool drawArrow;
-        private Texture2D lineTexture;
-        private Texture2D arrowTexture;
-        private Texture2D inheritanceArrowTexture;
-        
+        private bool isPointingTowardNode; //Only relevant for indicating arrows
         //length of the random connection
-        private const float randomConnectionLength = 200f;
 
         public ConnectionGUI(GraphManager graphManager, VisualElement footNode, VisualElement tipNode, bool isInheritanceConnection = false, bool setCenterLeft = false, bool drawArrow = true) : base(graphManager)
         {
@@ -31,43 +68,51 @@ namespace CodeExplorinator
             this.setCenterLeft = setCenterLeft;
             this.drawArrow = drawArrow;
             this.isInheritanceConnection = isInheritanceConnection;
-            lineTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/Linetexture.png");
-            arrowTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/pfeil_centered_new.png");
-            inheritanceArrowTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Graphics/Arrow_inheritance.png");
+            GenerateVisualElement();
+        }
+
+        public ConnectionGUI(GraphManager graphManager, VisualElement node, bool pointsTowardNode) : base(graphManager)
+        {
+            FootNode = node;
+            TipNode = null;
+            drawArrow = true;
+            isInheritanceConnection = false;
+            this.isPointingTowardNode = pointsTowardNode;
+            GenerateIndicatingVisualElement();
         }
 
         public override void GenerateVisualElement()
         {
-            
-            if (FootNode == null)
-            {
-                VisualElement = CreateRandomConnection(CenteredTopVector(TipNode), true, isInheritanceConnection);
-            }
-            else if (TipNode == null)
-            {
-                VisualElement = CreateRandomConnection(CenteredTopVector(FootNode), false, isInheritanceConnection);
-            }
-            else
-            {
-                VisualElement = CreateConnection(CenteredTopVector(FootNode), CenteredTopVector(TipNode), isInheritanceConnection);
-            }
+            VisualElement = CreateConnection(CalculateCenterTopPosition(FootNode), CalculateCenterTopPosition(TipNode), isInheritanceConnection);
+        }
+
+        private void GenerateIndicatingVisualElement()
+        {
+            Vector2 ankerpoint = CalculateCenterTopPosition(FootNode);
+            ankerpoint += CalculateIndicatingOffset();
+            VisualElement = CreateIndicatingConnection(ankerpoint, isPointingTowardNode);
         }
 
         public void UpdatePosition()
         {
             if (VisualElement != null)
             {
-                if (FootNode == null)
+                if (TipNode != null)
                 {
-                    UpdateRandomConnection(VisualElement,CenteredTopVector(TipNode), true, isInheritanceConnection);
+                    UpdateConnection(VisualElement, CalculateCenterTopPosition(FootNode),
+                                    CalculateCenterTopPosition(TipNode), isInheritanceConnection);
                 }
-                else if (TipNode == null)
+                else //else it is an indicating arrow
                 {
-                    UpdateRandomConnection(VisualElement,CenteredTopVector(FootNode), false, isInheritanceConnection);
-                }
-                else
-                { 
-                    UpdateConnection(VisualElement,CenteredTopVector(FootNode), CenteredTopVector(TipNode), isInheritanceConnection);
+                    Vector2 footPos = CalculateCenterTopPosition(FootNode) + CalculateIndicatingOffset();
+                    Vector2 tipPos = CalculateCenterTopPosition(FootNode) + CalculateIndicatingOffset() + new Vector2(0, -indicatingArrowLength);
+                    if(isPointingTowardNode)
+                    {
+                        Vector2 temp = footPos;
+                        footPos = tipPos;
+                        tipPos = temp;
+                    }
+                    UpdateConnection(VisualElement, footPos, tipPos, isInheritanceConnection);
                 }
             }
         }
@@ -79,73 +124,55 @@ namespace CodeExplorinator
 
         private VisualElement CreateConnection(Vector2 footPos, Vector2 tipPos, bool isInheritanceArrow)
         {
-            //creates parent visual element
-            VisualElement parent = new VisualElement();
-            
-            //creates the vector from node to node
-            Vector2 connection = new Vector2(tipPos.x - footPos.x, tipPos.y - footPos.y);
-            
-            //instantiates the line
+            VisualElement container = new VisualElement();
+            Vector2 footToTip = tipPos - footPos;
+
             VisualElement line = new VisualElement();
-            parent.Add(SetLinePosition(line,footPos,connection));
+            container.Add(SetLinePosition(line, footPos, footToTip));
 
             if (drawArrow)
             {
                 VisualElement arrow = new VisualElement();
-                parent.Add(SetArrowPosition(arrow, tipPos,connection, isInheritanceArrow ? inheritanceArrowTexture : arrowTexture));
+                container.Add(SetArrowPosition(arrow, tipPos, footToTip, isInheritanceArrow ? InheritanceArrowTexture : ArrowTexture));
             }
 
-            return parent;
+            return container;
         }
-        
-        private VisualElement CreateRandomConnection(Vector2 position, bool isIncomingArrow, bool isInheritanceArrow)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ankerpoint">The ankerpoint on the class on the top border of the class</param>
+        /// <param name="isIncomingArrow">If the arrow points to the class (or away from the class)</param>
+        /// <returns></returns>
+        private VisualElement CreateIndicatingConnection(Vector2 ankerpoint, bool isIncomingArrow)
         {
-            //creates parent visual element
-            VisualElement parent = new VisualElement();
-                
-            //creates random connection vector
-            Vector2 connection = new Vector2(Random.Range(-1000, 1000), Random.Range(-1000, 1000)).normalized * randomConnectionLength; //normalise it to the same length
-
-            //instantiates the line
+            VisualElement container = new VisualElement();
             VisualElement line = new VisualElement();
-            parent.Add(SetLinePosition(line,position,connection));
+            VisualElement arrowhead = new VisualElement();
+            Vector2 arrowAsVector = new Vector2(0, -indicatingArrowLength);
+            Vector2 arrowHeadPos = isIncomingArrow ? ankerpoint : (ankerpoint + arrowAsVector);
 
-            if (isIncomingArrow && drawArrow)
+            container.Add(SetLinePosition(line, ankerpoint, arrowAsVector));
+            if(!isIncomingArrow)
             {
-                VisualElement arrow = new VisualElement();
-                parent.Add(SetIncomingArrowPosition(arrow, position, connection, isInheritanceArrow ? inheritanceArrowTexture : arrowTexture));
+                arrowAsVector *= -1;
             }
-
-            return parent;
+            container.Add(SetIncomingArrowPosition(arrowhead, arrowHeadPos, arrowAsVector, ArrowTexture));
+            return container;
         }
 
         private void UpdateConnection(VisualElement parent, Vector2 footPos, Vector2 tipPos, bool isInheritanceArrow)
         {
-            //creates the vector from node to node
-            Vector2 connection = new Vector2(tipPos.x - footPos.x, tipPos.y - footPos.y);
+            Vector2 footToTip = tipPos - footPos;
             
-            SetLinePosition(parent.Children().First(),footPos,connection);
+            SetLinePosition(parent.Children().First(),footPos,footToTip);
 
             if (drawArrow)
             {
-                SetArrowPosition(parent.Children().Last(), tipPos,connection, isInheritanceArrow ? inheritanceArrowTexture : arrowTexture);
+                SetArrowPosition(parent.Children().Last(), tipPos,footToTip, isInheritanceArrow ? InheritanceArrowTexture : ArrowTexture);
             }
         }
-
-        private void UpdateRandomConnection(VisualElement parent, Vector2 position, bool isIncomingArrow, bool isInheritanceArrow)
-        {
-            //creates random connection vector
-            Vector2 connection = new Vector2(Random.Range(-1000, 1000), Random.Range(-1000, 1000)).normalized * randomConnectionLength; //normalise it to the same length
-            
-            SetLinePosition(parent.Children().First(),position,connection);
-
-            if (isIncomingArrow && drawArrow)
-            {
-                SetIncomingArrowPosition(parent.Children().Last(), position, connection, isInheritanceArrow ? inheritanceArrowTexture : arrowTexture);
-            }
-        }
-        
-        
 
         private VisualElement SetArrowPosition(VisualElement arrow, Vector2 v, Vector2 connection, Texture2D arrowTexture2D)
         {
@@ -168,7 +195,7 @@ namespace CodeExplorinator
             //centering v in the arrow image
             arrow.style.marginLeft = new StyleLength(u.x - arrowTexture2D.width * 0.5f);
             arrow.style.marginTop = new StyleLength(u.y - arrowTexture2D.height * 0.5f);
-            arrow.style.position = new StyleEnum<Position>(UnityEngine.UIElements.Position.Absolute);
+            arrow.style.position = new StyleEnum<Position>(Position.Absolute);
             arrow.style.height = arrowTexture2D.height;
             arrow.style.width = arrowTexture2D.height;
 
@@ -178,14 +205,14 @@ namespace CodeExplorinator
             return arrow;
         }
 
-        private VisualElement SetLinePosition(VisualElement line, Vector2 u, Vector2 connection)
+        private VisualElement SetLinePosition(VisualElement line, Vector2 ankerpoint, Vector2 connection)
         {
-            line.style.backgroundImage = new StyleBackground(lineTexture);
+            line.style.backgroundImage = new StyleBackground(LineTexture);
             //sets the rotationpoint of the line to the position u (the rotationpoint of the line lies in the middle) the line is not rotated yet, thats why we use the magnitude
-            line.style.marginLeft = new StyleLength(u.x - connection.magnitude * 0.5f);
-            line.style.marginTop = new StyleLength(u.y);
+            line.style.marginLeft = new StyleLength(ankerpoint.x - connection.magnitude * 0.5f);
+            line.style.marginTop = new StyleLength(ankerpoint.y);
             line.style.position = new StyleEnum<Position>(Position.Absolute);
-            line.style.height = lineTexture.height;
+            line.style.height = LineTexture.height;
             //the width of the line is the magnitude of the vector, as the line is not rotated yet
             line.style.width = connection.magnitude;
 
@@ -201,25 +228,18 @@ namespace CodeExplorinator
         }
 
         /// <summary>
-        /// creates vector v that holds the position of the node anchored centered on top
+        /// Calculates the relative center top position of the given node.
         /// </summary>
-        /// <param name="node">the node of which the position shall be calculated</param>
-        /// <returns></returns>
-        private Vector2 CenteredTopVector(VisualElement node)
+        /// <param name="node">The node of which the position is to be calculated</param>
+        /// <returns>The center top position of the given node</returns>
+        private Vector2 CalculateCenterTopPosition(VisualElement node)
         {
-            return new Vector2(
-                node.style.marginLeft.value.value +
-                node.style.width.value.value * 0.5f,
-                node.style.marginTop.value.value);
-        }
-        
-        private Vector2 CenteredLeftVectorForMethod(VisualElement node)
-        {
-            return new Vector2(node.parent.style.marginLeft.value.value + (
-                node.style.marginLeft.value.value),
-                node.parent.style.marginTop.value.value + (
-                    node.style.marginTop.value.value + node.style.height.value.value * 0.5f));
+            return new Vector2(node.style.marginLeft.value.value + node.style.width.value.value * 0.5f, node.style.marginTop.value.value);
         }
 
+        private Vector2 CalculateIndicatingOffset()
+        {
+            return new Vector2(isPointingTowardNode ? -indicatingArrowsOffset : indicatingArrowsOffset, 0);
+        }
     }
 }
